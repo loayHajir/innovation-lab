b30d148fcfad106bd15a84673e1902e6bf5240b1
zeinab <zeinabmiari4@gmail.com>
2024-03-20 10:58:30 +0200

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Login'
 
 function App() {
 
 
   return (
     <>
-    <h1>You Are Donkey</h1>
-    <p>hello world!</p>
+    <Login />
     </>
   )
 }
